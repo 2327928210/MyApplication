5c0179d77a38f0d7a7913cfd2169c4a6fef8df1f
zhangqiang <2327928210@qq.com>
2017-03-26 11:10:54 +0800

    添加了My类

---
+++ app/src/main/java/My.java
@@ -0,0 +1,13 @@
+import android.app.Application;
+
+/**
+ * Created by haier on 2017/3/26.
+ */
+
+public class My extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
